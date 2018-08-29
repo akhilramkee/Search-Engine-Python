fb725a6627c00fa48651bf7005bbcb4597afb615
akhilramkee <akhilramkee123@gmail.com>
2018-08-29 08:02:50 +0530

    Added a crawler that automatically crawls and appends itself
    Correct it if necessary

--- Crawl.py
+++ Crawl.py
@@ -1,5 +1,25 @@
 from bs4 import BeautifulSoup
-import requests
-import requests.exceptions
-from urllib.parse import urlsplit
-from collections import deque
+from urllib.request import urlopen
+
+#Links to be crawled
+linklist=['https://www.pdfdrive.com']
+
+#Crawling process
+for itr in linklist:
+    
+    print("Going into the link:" +itr)
+    #Open the required url in the redditFile
+    redditFile=urlopen(itr)
+    redditHtml=redditFile.read()
+    redditFile.close()
+    
+    soup=BeautifulSoup(redditHtml,features="lxml")
+    redditAll=soup.find_all("a")
+    for links in soup.select("a"):
+        temp=links.get('href')
+        temp1=itr+temp
+        if temp1.endswith('pdf'):
+            print("Pdf file:")
+        else:
+            if temp1.endswith('html'):
+                linklist.append(temp1)
